2ef1a729855facb9e4679a04ff7f9b3ef00c0152
CloudlyBot | Argantiu <114483978+CloudlyBot@users.noreply.github.com>
2024-02-07 09:15:12 +0100

    Update PerPlayerWorld.java

--- src/main/java/de/argantiu/perplayerworld/PerPlayerWorld.java
+++ src/main/java/de/argantiu/perplayerworld/PerPlayerWorld.java
@@ -1,7 +1,14 @@
-package de.argantiu.PerPlayerWorld
+package de.argantiu.perplayerworld;
 
-//import de.argantiu.perplayerworld.commands
+//import de.argantiu.perplayerworld.cmd
 //import de.argantiu.perplayerworld.gui
 
 public class PerPlayerWorld extends JavaPlugin {
+    private static boolean is1_13Plus = false;
+    final private String version = this.getDescription().getVersion();
+    private CreatorAdapter creator;
   
+  public static void createConfigs() {
+    File folder = getInstance().getDataFolder();
+    File dir = new File(folder + "/worlds");
+    
